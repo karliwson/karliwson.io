2324ce0227a81893216b8370416613547640b1da
Kaká Oliveira <me@karliwson.io>
2022-07-22 18:56:47 -0300

    Author info: replace twitter link with LinkedIn

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -74,12 +74,15 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
                         <dd className="text-gray-900 dark:text-gray-100">{author.name}</dd>
                         <dt className="sr-only">Twitter</dt>
                         <dd>
-                          {author.twitter && (
+                          {author.linkedin && (
                             <Link
-                              href={author.twitter}
+                              href={author.linkedin}
                               className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
                             >
-                              {author.twitter.replace('https://twitter.com/', '@')}
+                              {author.linkedin.replace(
+                                /https:\/\/(www\.)?linkedin\.com\/in\//,
+                                'in/' // '@'
+                              )}
                             </Link>
                           )}
                         </dd>
